555b93b5d740b0e9c664eaee499c82d0060baf17
kaiser18 <oliahim.ivic@gmail.com>
2025-04-01 17:20:57 +0200

    added assets, started work on tab navigation

---
+++ app/films/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const FilmDetails = () => {
+  return (
+    <View>
+      <Text>Film details</Text>
+    </View>
+  )
+}
+
+export default FilmDetails
